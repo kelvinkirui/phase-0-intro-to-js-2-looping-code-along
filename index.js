e24aa538f01c58f052418c6baddacc213c68948c
kk <+>
2023-03-12 21:33:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const messag = [];
+function writeCards(messag, event) {
+  for (let i = 0; i < messag.length; i++) {
+      console.log(`Thank you, ${messag[i]}, for the wonderful ${event} gift!`)
+  }
+  return messag;
+  
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+function countDown(intNum) {
+    while (intNum >= 0) {
+      console.log(intNum);
+      intNum--;
+    }
+    return intNum;
+  }
+  countDown(10);
